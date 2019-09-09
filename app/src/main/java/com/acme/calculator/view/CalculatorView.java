182be13ae5bfc8bdf51f308746d63c78845a8b19
Alejandro Torrico <a.torricomoreno@gmail.com>
2019-09-08 19:21:19 -0500

    change the layout to calculator

---
+++ app/src/main/java/com/acme/calculator/view/CalculatorView.java
@@ -0,0 +1,8 @@
+package com.acme.calculator.view;
+
+public interface CalculatorView {
+    void showWinner(String winningPlayerDisplayLabel);
+    void clearWinnerDisplay();
+    void clearButtons();
+    void setButtonText(int row, int col, String text);
+}
